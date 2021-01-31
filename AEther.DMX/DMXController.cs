7acc76d3a1f207d2e95b306f4de2ed38f178701b
Volker Schmidt <volker.schmidt91@gmail.com>
2021-01-31 21:33:25 +0100

    sync

--- AEther.DMX/DMXController.cs
+++ AEther.DMX/DMXController.cs
@@ -17,8 +17,12 @@ namespace AEther.DMX
         readonly CancellationTokenSource Cancel;
         readonly byte[] Frame;
 
+        readonly MovingQuantileEstimator TransientFilter;
+
         public DMXController(string comPort)
         {
+            TransientFilter = new MovingQuantileEstimator(.9f, .005f, 0f);
+            var ports = SerialPort.GetPortNames();
             Serial = new SerialPort(comPort, Baudrate)
             {
                 DataBits = 8,
@@ -35,12 +39,11 @@ namespace AEther.DMX
         {
             while (!cancel.IsCancellationRequested)
             {
-                var length = Array.FindLastIndex(Frame, x => 0 < x) switch
+                var lastIndex = Array.FindLastIndex(Frame, x => 0 < x);
+                if (lastIndex != -1)
                 {
-                    -1 => Frame.Length,
-                    var i => i + 1,
-                };
-                Serial.Write(Frame, 0, length);
+                    Serial.Write(Frame, 0, lastIndex + 1);
+                }
             }
         }
 
@@ -54,21 +57,23 @@ namespace AEther.DMX
             }
             for (var i = 0; i < sum.Length; ++i)
             {
-                sum[i] *= sum.Length / evt.SampleCount;
+                sum[i] *= sum.Length / (float)evt.SampleCount;
             }
-            var sinuoids = (byte)(255 * sum[0]);
-            var transients = (byte)(255 * sum[1]);
-            var noise = (byte)(255 * sum[3]);
-            var residual = (byte)(255 * sum[4]);
-            Frame[1] = transients;
-            Frame[2] = transients;
+            var sinuoids = (byte)(5f * 255 * sum[0]);
+            var transientFiltered = TransientFilter.Filter(sum[1]);
+            var transients = transientFiltered < sum[1];
+            var noise = (byte)(255 * sum[2]);
+            var residual = (byte)(255 * sum[3]);
+            Frame[1] = noise;
+            Frame[2] = noise;
             Frame[3] = 0;
             Frame[4] = noise;
             Frame[5] = 0;
             Frame[6] = sinuoids;
             Frame[7] = 0;
             Frame[8] = sinuoids;
-            Frame[9] = transients;
+            Frame[9] = transients ? 20 : 200;
+            //Frame[9] = (byte)(15f * 255 * sum[1]);
             Frame[10] = 0;
         }
 
